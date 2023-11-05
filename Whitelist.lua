7a65892f4dde9a42838bdfa68b5016007f4e9a7c
Zbossgamer <82695203+Zbossgamer@users.noreply.github.com>
2023-11-05 17:59:00 -0600

    Player Added: AfterFluff

--- Whitelist.lua
+++ Whitelist.lua
@@ -53,5 +53,6 @@ Whitelist = {
   --11/4
   5203096606, --TrpJake / 74Gear (800R$)
   --11/5
-  825789867 --CladRacer / Nemu (599R$)
+  825789867, --CladRacer / Nemu (599R$)
+  329554436 --AfterFluff (599R$)
 }
