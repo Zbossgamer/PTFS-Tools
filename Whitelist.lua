caa5f4b807362f496a21c6e3f4be37eb4c2adc48
Zbossgamer <82695203+Zbossgamer@users.noreply.github.com>
2023-11-17 16:19:20 -0600

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -61,5 +61,8 @@ Whitelist = {
   940299930, --Wolf (599R$)
   --11/11
   143724606, --loffu /coy (599R$)
-  3311667111 --dolphin 10$ mazon gift card)
+  3311667111, --dolphin 10$ mazon gift card)
+  --11/16
+  880025886 --royumu (599R$
+)
 }
